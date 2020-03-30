9f5eaf5f838cdb0bb81ee32b13731acf18280846
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-03-30 18:52:20 +0000

    core: exercise completed

--- js-dom/app.js
+++ js-dom/app.js
@@ -1,14 +1,23 @@
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
-const resetButton = document.getElementById('resetButton')
 
-// changing the color of the heading
-myButton.addEventListener('click',()=>{
-  myHeading.style.color = myTextInput.value;
-});
+// exercise 1
+// const myHeading = document.getElementById('myHeading');
+// const myButton = document.getElementById('myButton');
+// const myTextInput = document.getElementById('myTextInput');
+// const resetButton = document.getElementById('resetButton')
 
-//event listener for reset button
-resetButton.addEventListener('click',()=>{
-  myHeading.style.color = 'black';
-});
+// // changing the color of the heading
+// myButton.addEventListener('click',()=>{
+//   myHeading.style.color = myTextInput.value;
+// });
+
+// //event listener for reset button
+// resetButton.addEventListener('click',()=>{
+//   myHeading.style.color = 'black';
+// });
+
+
+
+// exercise 2 changing content of a tag
+const myHeading = document.querySelector('h3');
+
+myHeading.textContent = "This is a new Heading";
